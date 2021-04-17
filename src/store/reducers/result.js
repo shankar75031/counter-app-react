3df6071149c778a708300412bb6585191755f6b8
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-17 22:20:04 +0530

    refactor switch cases in reducers

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,23 +1,24 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 const initialState = {
   results: [],
+};
+
+const deleteResult = (state, action) => {
+  const newArray = state.results.filter((element) => {
+    return element.id !== action.resultElId;
+  });
+  return updateObject(state, { results: newArray });
 };
 
 const resultReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ value: action.result, id: new Date() }),
-      };
-    case actionTypes.DELETE_RESULT:
-      const newArray = state.results.filter((element) => {
-        return element.id !== action.resultElId;
       });
-      return {
-        ...state,
-        results: newArray,
-      };
+    case actionTypes.DELETE_RESULT:
+      return deleteResult(state, action);
     default:
       return state;
   }
