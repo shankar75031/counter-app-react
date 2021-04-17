be4b20e66355d1a55beec06a3690e2b2fa4e545f
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-17 17:46:06 +0530

    async action creators

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -33,10 +33,18 @@ export const subtract = (value) => {
   };
 };
 
-export const storeResult = (res) => {
+export const saveResult = (res) => {
   return {
     type: STORE_RESULT,
     result: res,
+  };
+};
+
+export const storeResult = (res) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(saveResult(res));
+    }, 2000);
   };
 };
 
